b53bb23426d9c44d8824cd2ccfc95100473087c6
Олег Сайфутдинов <zeusfly@mail.ru>
2025-11-24 21:20:53 +0300

    Start

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class AboutController extends AbstractController
+{
+    #[Route('/about') ]
+    public function about(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('about.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
